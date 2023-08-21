cbeaadfaebccf98ce899f2a0e5aaba63b165b32c
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-08-21 12:56:10 +0000

    adding authentication

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -49,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = asyncHandler(async (req, res) => {
+  const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
   // Check for user email
